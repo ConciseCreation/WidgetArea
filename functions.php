e085f74baabf3b597aab4735c9e560164d25b964
ConciseCreation <33875961+ConciseCreation@users.noreply.github.com>
2017-11-21 19:04:39 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,5 @@
+genesis_register_sidebar( array(
+	'id'          => 'my-widget',
+	'name'        => __( 'My widget', 'theme-name' ),
+	'description' => __( 'This is your first widget.', 'theme-name' ),
+) );
